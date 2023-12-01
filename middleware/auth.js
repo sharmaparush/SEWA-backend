b184108a977d3b65b5595b5a535ad0ec16edb400
sharmaparush <99115759+sharmaparush@users.noreply.github.com>
2023-12-01 23:14:13 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,8 @@
 var jwt = require('jsonwebtoken');
 const jwtkey=require('../keys');
 const auth=(req,res,next)=>{
-  if(req.cookies.jwt){
-    const check=jwt.verify(req.cookies.jwt,jwtkey);
+  if(req.body.jwt){
+    const check=jwt.verify(req.body.jwt,jwtkey);
     req.user={"id":check.data}
     next();
   }
